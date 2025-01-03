11c7adf19069a8d1a22fa0e403fb7e20b6cd6fd0
Olga Yefremova <lefrolg@gmail.com>
2025-01-03 12:37:34 +0200

    improved MovieView layout

--- src/views/MovieView.vue
+++ src/views/MovieView.vue
@@ -34,76 +34,68 @@ const imagePath = import.meta.env.VITE_TMDB_IMAGE_URL_BIG;
 </script>
 
 <template>
-  <v-sheet
-      border="none"
-      class="text-body-2 mx-auto"
-      max-width="1200"
-  >
-    <v-container fluid>
-      <v-row>
-        <v-col cols="12" md="3">
-          <v-img
-              :src="imagePath + movie?.poster_path"
-              aspect-ratio="16/9"
-              class="align-end text-white rounded-lg"
-              cover
-          >
-            <template v-slot:placeholder>
-              <div class="d-flex align-center justify-center fill-height">
-                <v-progress-circular
-                    color="grey-lighten-4"
-                    indeterminate
-                ></v-progress-circular>
-              </div>
-            </template>
-          </v-img>
-        </v-col>
-
-        <v-col cols="12" md="9" v-if="!isLoading">
-          <h1 v-if="movie.id"> {{ movie.title }}
-            <template v-if="movieYear">
-              ({{ movieYear }})
-            </template>
-          </h1>
-          <p class="mb-2"><i>{{ movie.tagline }}</i></p>
-          <div class="d-flex align-center ga-2 mb-4" v-if="movie.vote_average">
-            <v-rating
-                :length="10"
-                :model-value="movie.vote_average"
-                active-color="orange"
-                hover
-                readonly
-                size="small"
-            />
-            <div class="rating-number">
-              {{ movie.vote_average }} ({{ movie.vote_count }})
+  <div class="mb-4">
+    <v-row>
+      <v-col cols="12" md="3">
+        <v-img
+          :src="imagePath + movie?.poster_path"
+          aspect-ratio="16/9"
+          class="align-end text-white rounded-lg"
+          cover
+        >
+          <template v-slot:placeholder>
+            <div class="d-flex align-center justify-center fill-height">
+              <v-progress-circular
+                color="grey-lighten-4"
+                indeterminate
+              ></v-progress-circular>
             </div>
+          </template>
+        </v-img>
+      </v-col>
+
+      <v-col v-if="!isLoading" cols="12" md="9">
+        <h1 v-if="movie.id"> {{ movie.title }}
+          <template v-if="movieYear">
+            ({{ movieYear }})
+          </template>
+        </h1>
+        <p class="mb-2"><i>{{ movie.tagline }}</i></p>
+        <div v-if="movie.vote_average" class="d-flex align-center ga-2 mb-4">
+          <v-rating
+            :length="10"
+            :model-value="movie.vote_average"
+            active-color="orange"
+            hover
+            readonly
+            size="small"
+          />
+          <div class="rating-number">
+            {{ movie.vote_average }} ({{ movie.vote_count }})
           </div>
-          <div v-if="genres" class="mb-2">
-            <span class="pr-2 font-weight-bold">Genres:</span> {{ genres }}
-          </div>
-          <div v-if="countries" class="mb-4">
-            <span class="pr-2 font-weight-bold">Countries:</span> {{ countries }}
-          </div>
-          <div v-if="movie.overview">{{ movie.overview }}</div>
-        </v-col>
-      </v-row>
-    </v-container>
-  </v-sheet>
-  <v-container>
-    <h2 class="mb-2">Similar movies</h2>
-    <v-row v-if="similarMovies.length">
-      <v-col v-for="movie of similarMovies" :key="movie?.id" cols="3">
-        <movie-card
-            :id="movie?.id"
-            :poster-url="movie?.poster_path || ''"
-            :rating="movie?.vote_average || 0"
-            :release-date="movie?.release_date || ''"
-            :title="movie?.title || ''"
-        />
+        </div>
+        <div v-if="genres" class="mb-2">
+          <span class="pr-2 font-weight-bold">Genres:</span> {{ genres }}
+        </div>
+        <div v-if="countries" class="mb-4">
+          <span class="pr-2 font-weight-bold">Countries:</span> {{ countries }}
+        </div>
+        <div v-if="movie.overview">{{ movie.overview }}</div>
       </v-col>
     </v-row>
-  </v-container>
+  </div>
+  <h2 class="mb-2">Similar movies</h2>
+  <v-row v-if="similarMovies.length">
+    <v-col v-for="movie of similarMovies" :key="movie?.id" cols="3">
+      <movie-card
+        :id="movie?.id"
+        :poster-url="movie?.poster_path || ''"
+        :rating="movie?.vote_average || 0"
+        :release-date="movie?.release_date || ''"
+        :title="movie?.title || ''"
+      />
+    </v-col>
+  </v-row>
 
 </template>
 
